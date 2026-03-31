f3a36656fe89046518df9fe45890570e92308b92
Sandraoptimas <sandra.ca@optimas.ai>
2026-03-31 17:45:59 +0530

    Added search by city feature using ArrayList
    Arraylist2-Added search by city feature using ArrayList

---
+++ Arraylist2/src/com/optimasai/empservice/test/ContactOps.java
@@ -0,0 +1,10 @@
+package com.optimasai.empservice.test;
+
+public class ContactOps {
+
+    public static void main(String[] args) {
+
+        ContactRepo repo = new ContactRepo();
+        repo.menu();
+    }
+}
